05c60bfc4bc263b54c7bee85210949bb0316c0fa
BoiBobbyBo <144557362+BoiBobbyBo@users.noreply.github.com>
2024-11-13 04:16:07 +0200

    Component diagram update

--- src/pages/component/ComponentPage.jsx
+++ src/pages/component/ComponentPage.jsx
@@ -1,7 +1,10 @@
 import { PureComponent as Component } from "react";
-import myImage from "./component.drawio.png";
-import myImage2 from "./component2.drawio.png";
-import myImage3 from "./component3.drawio.png";
+import myImage from './component.drawio.png';
+import myImage2 from './component2.drawio.png';
+import myImage3 from './component3.drawio.png';
+import myImage4 from './1.png';
+import myImage5 from './2.png';
+import myImage6 from './3.png';
 
 class ComponentPage extends Component {
   constructor(props) {
@@ -9,111 +12,190 @@ class ComponentPage extends Component {
   }
 
   render() {
-    return (
+    return ( 
+      <div style={{ textAlign: "center", padding: "3rem 0" }}>
+
+    <h1  style={{padding: "2rem 0 1rem 0", fontSize: "3rem", fontWeight: "bold",
+    }}>Komponenšu diagramma</h1>
+
+    <h1  style={{padding: "0rem 0 4rem 0", fontSize: "1.5rem" , fontWeight: "bold",
+    }}>Strukturas (structural)</h1>
+
+<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: '20px', padding: '20px' }}>
+
+  <div style={{width: "700px"}}>
+    <h1  style={{padding: "0rem 0 3rem 0", fontSize: "2rem", fontWeight: "bold",}}>Kas ir komponenšu diagramma</h1>
+    <p>Komponenšu diagramma ir viena no UML (Unified Modeling Language) diagrammām, kas tiek izmantota, lai attēlotu sistēmas struktūru un tās komponentes, kā arī to savstarpējās attiecības.</p>
+  </div>
+  <div>
+  <img
+    src={myImage} // Use the imported image
+    alt="Komponenšu diagramma example"
+    style={{
+      display: "block",
+      margin: "auto",
+      width: "900px",
+      height: "auto",
+      borderRadius: "10px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"
+    }}
+  />
+  </div>
+</div>
+{/*--------------------------------- */}
+
+<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', gap: '20px', padding: '4rem 20px 20px 20px' }}>
+
+<div>
+    <h1  style={{padding: "0rem 0 3rem 0", fontSize: "2rem", fontWeight: "bold",}}>Viņas mērķis/kam to izmantot?</h1>
+    <p>Komponentu diagrammas tiek izmantotas, lai viegli pārskatītu lielas un sarežģītas sistēmas no augstāka līmeņa, kā arī lai sadalītu lielus projektus mazākās, pārskatāmākās daļās, tādējādi atvieglojot orientēšanos un sapratni par projekta struktūru.</p>
+  </div>
+      
+      <div style={{ display: 'flex', flexDirection: 'column', padding: '0 5rem 0 0', gap: '20px' }}>
+      <div style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '15px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '250px'
+        }}>
+          <h3 style={{ margin: 0 }}>Modularitāte</h3>
+        </div>
+        <div style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '15px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '250px'
+        }}>
+          <h3 style={{ margin: 0 }}>Sistēmas struktūras izpratne</h3>
+        </div>
+        <div style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '15px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '250px'
+        }}>
+          <h3 style={{ margin: 0 }}>Izmaiņu ieviešana</h3>
+        </div>
+        <div style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '15px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '250px'
+        }}>
+          <h3 style={{ margin: 0 }}>Komponentu aizvietošana</h3>
+        </div>
+      </div>
+    </div>
+
+{/*--------------------------------- */}
+
+
+<h1  style={{padding: "0rem 0 3rem 0", fontSize: "2rem", fontWeight: "bold",}}>Kādas problēmas risina komponenšu diagramma?</h1>
+
+<div style={{ display: 'flex', flexDirection: 'row', gap: '20px', padding: '20px' }}>
+      {/* Card 1 */}
       <div
         style={{
-          textAlign: "center",
-          padding: "3rem 2rem",
-          background: "white",
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '10px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '400px',
         }}
       >
-        <h1
-          style={{
-            padding: "2rem 0 1rem 0",
-            fontSize: "3rem",
-            fontWeight: "bold",
-          }}
-        >
-          Komponenšu diagramma
-        </h1>
-
-        <h1
-          style={{
-            padding: "0rem 0 2rem 0",
-            fontSize: "1.5rem",
-            fontWeight: "bold",
-          }}
-        >
-          Strukturas (structural)
-        </h1>
-
-        <h1 style={{ padding: "0rem 0 1rem 0", fontSize: "1.5rem" }}>
-          Kas tā tāda ir?
-        </h1>
-
-        <p style={{ padding: "0rem 0 2rem 0" }}>
-          Komponenšu diagrammas ir struktūras diagrammas, kas parāda sistēmas
-          komponentes un to savstarpējās attiecības.
-        </p>
-
-        <img
-          src={myImage} // Use the imported image
-          alt="Komponenšu diagramma example"
-          style={{
-            display: "block",
-            margin: "auto",
-            width: "1000px",
-            height: "auto",
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
-        />
-
-        <h1 style={{ padding: "2rem 0 1rem 0", fontSize: "1.5rem" }}>
-          Viņas mērķis/kam to izmantot?
-        </h1>
-
-        <p style={{ padding: "0rem 0 2rem 0" }}>
-          Komponentu diagrammas tiek izmantotas, lai viegli pārskatītu lielas un
-          sarežģītas sistēmas no augstāka līmeņa, kā arī lai sadalītu lielus
-          projektus mazākās, pārskatāmākās daļās, tādējādi atvieglojot
-          orientēšanos un sapratni par projekta struktūru.
-        </p>
-
-        <img
-          src={myImage2} // Use the imported image
-          alt="Komponenšu diagramma example"
-          style={{
-            display: "block",
-            margin: "auto",
-            width: "1000px",
-            height: "auto",
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
-        />
-
-        <h1 style={{ padding: "3rem 0 1rem 0", fontSize: "1.5rem" }}>
-          Īss video
-        </h1>
-        <iframe
-          width="100%" // Makes the iframe responsive
-          height="650" // Adjust the height as needed
-          src="https://www.youtube.com/embed/U0ssF7mvTk8" // YouTube embed URL
-          title="YouTube video"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-
-        <h1 style={{ padding: "3rem 0 1rem 0", fontSize: "1.5rem" }}>
-          Piemērs ar dabas elementiem
-        </h1>
-        <img
-          src={myImage3} // Use the imported image
-          alt="Komponenšu diagramma example"
-          style={{
-            display: "block",
-            margin: "auto",
-            width: "1000px",
-            height: "auto",
-            borderRadius: "10px",
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          }}
-        />
+        <h3 style={{ margin: 0, fontSize: '24px', borderBottom: '2px solid #ddd', paddingBottom: '10px' }}>
+        Sistēmas sarežģītības pārvaldība
+        </h3>
+        <p> Lielas sistēmas bieži vien ir ļoti sarežģītas, ar daudzām mijiedarbībām starp dažādām sastāvdaļām.</p>
       </div>
-    );
-  }
+
+      {/* Card 2 */}
+      <div
+        style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '10px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '400px',
+        }}
+      >
+        <h3 style={{ margin: 0, fontSize: '24px', borderBottom: '2px solid #ddd', paddingBottom: '10px' }}>
+        Komponentu atkarību identifikācija
+        </h3>
+        <p>Bez skaidras atkarību izpratnes var būt grūti izdarīt izmaiņas sistēmā, neizraisot nevēlamas sekas.</p>
+      </div>
+
+      {/* Card 3 */}
+      <div
+        style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '10px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '400px',
+        }}
+      >
+        <h3 style={{ margin: 0, fontSize: '24px', borderBottom: '2px solid #ddd', paddingBottom: '10px' }}>
+        Izstrādes un uzturēšanas laika samazināšana
+        </h3>
+        <p>Projektējot un uzturot sistēmas, ir svarīgi novērst lieku darbu un neskaidrības.</p>
+      </div>
+
+      {/* Card 4 */}
+      <div
+        style={{
+          border: '1px solid white', // White border
+          borderRadius: '8px',
+          padding: '10px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          backgroundColor: 'none', // No background color
+          width: '400px',
+        }}
+      >
+        <h3 style={{ margin: 0, fontSize: '24px', borderBottom: '2px solid #ddd', paddingBottom: '10px' }}>
+        Kompleksu sistēmu uzturēšanas vienkāršošana
+        </h3>
+        <p>Lielas un sarežģītas sistēmas prasa biežu uzturēšanu un izmaiņas, kas var būt izaicinājums.</p>
+      </div>
+    </div>
+{/*--------------------------------- */}
+    <h1 style={{padding: "3rem 0 1rem 0", fontSize: "2rem", fontWeight: "bold" ,
+    }}>Komponentes pamatdaļas/elementi</h1>
+    <img
+    src={myImage2} // Use the imported image
+    alt="Komponenšu diagramma example"
+    style={{
+      display: "block",
+      margin: "auto",
+      width: "900px",
+      height: "auto",
+      borderRadius: "10px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)"
+    }}
+  />
+
+
+<h1 style={{padding: "3rem 0 1rem 0", fontSize: "2rem", fontWeight: "bold" ,
+    }}>Vai tu atceries ko tu izlasiji?</h1>
+<iframe src="https://s8wrb6th95u.typeform.com/to/xHnRsbqt" width="100%" height="500px" frameborder="0"></iframe>
+
+  </div>
+
+    )
+    
+  };
+
 }
 
 export default ComponentPage;
